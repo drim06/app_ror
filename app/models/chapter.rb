5f03f0855349c75e81d47dec9eacb89da4eec755
Marc Destefanis <marc.destefanis.etu@gmail.com>
2014-05-28 13:29:59 -0700

    An user can Create a Chapter and see all chapters

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,6 +1,6 @@
 class Chapter < ActiveRecord::Base
-  validates :title, presence: true, length: { maximum: 50 }
   validates :number, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
   validates :scene, presence: true
   validates :anecdote, presence: true
 end
